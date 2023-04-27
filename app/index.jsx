63c498c36cc2a11f9eafac9dbea35fdba686e77e
Sam Elwitt <sam.elwitt@ibm.com>
2023-04-27 13:35:19 -0400

    demo environment var

--- app/index.jsx
+++ app/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './components/App/App';
-
+console.log(process.env.NODE_ENV);
 const render = () => {
   ReactDOM.render(
       <Router>
